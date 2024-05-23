fc152285ac94db2217a2bc3b8c00106614a48a21
Fromero564 <fervicromero@gmail.com>
2024-05-23 20:34:53 -0300

    Pagina de rostock

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./estilos/normalize.css" />
+    <link rel="stylesheet" type="text/css" href="estilos/style.css" />
+    <title>Rostock</title>
+</head>
+<body>
+    <header class="header-rostock">
+        <img src="" alt="">
+        <form action="">
+            <input type="Busqueda">
+            <button type="submit">Lupa</button>
+        </form>
+        <nav>
+        <ul>
+            <li>Home</li>
+            <li>Ingresar</li>
+            <li>Carrito</li>
+        </ul>
+        </nav>
+    </header>
+    <main>
+     <section>
+        <p>Categorias</p>
+     </section>
+     <section>
+        <article>
+            <p>Populares</p>
+        </article>
+        <article>
+             <p>Todos los productos</p>
+        </article>
+     </section>
+    </main>
+</body>
+<footer>
+
+</footer>
+</html>
