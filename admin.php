94cf67e6900810199240e02a431e9123d2e98ba0
Fromero564 <fervicromero@gmail.com>
2024-08-19 12:23:23 -0300

    Se agregan archivos

---
+++ admin.php
@@ -0,0 +1,10 @@
+<html>
+<?php
+require_once    'login.php';
+
+$conn=new mysqli($hn,$un,$pw,$db);
+
+if($conn->connect_error)die("Fatal Error");
+?>
+
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -27,9 +28,9 @@
     <section class="seccion-portada">
     <article class="portada">
     <div class="texto-bienvenida">
-          <div class="div-texto">
+          <!-- <div class="div-texto">
              <p id="fade-text">¡Bienvenidos a Rostock!</p> 
-             </div>
+             </div> -->
      </div>
         <img src="./img/banner/Banner.jpeg" alt="FotoBannerRostock" class="img-1">
         <img src="./img/banner/Banner1.jpeg" alt="FotoBannerRostock" class="img-2">
@@ -46,58 +47,43 @@
      <section style="width:15vw;background-color:#325899;margin:25px;height:70vh">
         <p>Categorias</p>
      </section>
-     <section style="width:80vw;margin:25px">
-        <article style="display:flex;justify-content:space-around;align-items:center">
-        <div class="card">
-        <div class="card-img"><div class="img"></div></div>
-        <div class="card-title">Product title</div>
-        <div class="card-subtitle">Product description. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
-        <hr class="card-divider">
-        <div class="card-footer">
-        <div class="card-price"><span>$</span> 123.45</div>
-        <button class="card-btn">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="m397.78 316h-205.13a15 15 0 0 1 -14.65-11.67l-34.54-150.48a15 15 0 0 1 14.62-18.36h274.27a15 15 0 0 1 14.65 18.36l-34.6 150.48a15 15 0 0 1 -14.62 11.67zm-193.19-30h181.25l27.67-120.48h-236.6z"></path><path d="m222 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path><path d="m368.42 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path><path d="m158.08 165.49a15 15 0 0 1 -14.23-10.26l-25.71-77.23h-47.44a15 15 0 1 1 0-30h58.3a15 15 0 0 1 14.23 10.26l29.13 87.49a15 15 0 0 1 -14.23 19.74z"></path></svg>
-        </button>
-       </div>
-        </div>
-        <div class="card">
-        <div class="card-img"><div class="img"></div></div>
-        <div class="card-title">Product title</div>
-        <div class="card-subtitle">Product description. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
-        <hr class="card-divider">
-        <div class="card-footer">
-        <div class="card-price"><span>$</span> 123.45</div>
-        <button class="card-btn">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="m397.78 316h-205.13a15 15 0 0 1 -14.65-11.67l-34.54-150.48a15 15 0 0 1 14.62-18.36h274.27a15 15 0 0 1 14.65 18.36l-34.6 150.48a15 15 0 0 1 -14.62 11.67zm-193.19-30h181.25l27.67-120.48h-236.6z"></path><path d="m222 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path><path d="m368.42 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path><path d="m158.08 165.49a15 15 0 0 1 -14.23-10.26l-25.71-77.23h-47.44a15 15 0 1 1 0-30h58.3a15 15 0 0 1 14.23 10.26l29.13 87.49a15 15 0 0 1 -14.23 19.74z"></path></svg>
-        </button>
-       </div>
-        </div>
-        <div class="card">
-        <div class="card-img"><div class="img"></div></div>
-        <div class="card-title">Product title</div>
-        <div class="card-subtitle">Product description. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
-        <hr class="card-divider">
-        <div class="card-footer">
-        <div class="card-price"><span>$</span> 123.45</div>
-        <button class="card-btn">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="m397.78 316h-205.13a15 15 0 0 1 -14.65-11.67l-34.54-150.48a15 15 0 0 1 14.62-18.36h274.27a15 15 0 0 1 14.65 18.36l-34.6 150.48a15 15 0 0 1 -14.62 11.67zm-193.19-30h181.25l27.67-120.48h-236.6z"></path><path d="m222 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path><path d="m368.42 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path><path d="m158.08 165.49a15 15 0 0 1 -14.23-10.26l-25.71-77.23h-47.44a15 15 0 1 1 0-30h58.3a15 15 0 0 1 14.23 10.26l29.13 87.49a15 15 0 0 1 -14.23 19.74z"></path></svg>
-        </button>
-       </div>
-        </div>
-        <div class="card">
-        <div class="card-img"><div class="img"></div></div>
-        <div class="card-title">Product title</div>
-        <div class="card-subtitle">Product description. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
-        <hr class="card-divider">
-        <div class="card-footer">
-        <div class="card-price"><span>$</span> 123.45</div>
-        <button class="card-btn">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="m397.78 316h-205.13a15 15 0 0 1 -14.65-11.67l-34.54-150.48a15 15 0 0 1 14.62-18.36h274.27a15 15 0 0 1 14.65 18.36l-34.6 150.48a15 15 0 0 1 -14.62 11.67zm-193.19-30h181.25l27.67-120.48h-236.6z"></path><path d="m222 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path><path d="m368.42 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path><path d="m158.08 165.49a15 15 0 0 1 -14.23-10.26l-25.71-77.23h-47.44a15 15 0 1 1 0-30h58.3a15 15 0 0 1 14.23 10.26l29.13 87.49a15 15 0 0 1 -14.23 19.74z"></path></svg>
-        </button>
-       </div>
-        </div>
-        </article>
-     </section>
+     <section style="width:80vw;margin:25px;display: flex;flex-wrap: wrap;gap: 20px;">
+    <?php
+    require_once 'login.php';
+    $conn = new mysqli($hn, $un, $pw, $db);
+    if ($conn->connect_error) die("Fatal Error");
+  
+    
+    $query = "SELECT * FROM productos";
+    $result = $conn->query($query);
+
+    if (!$result) die("Fatal Error");
+
+    $rows = $result->num_rows;
+
+    for ($j = 0; $j < $rows; ++$j) {
+        $row = $result->fetch_array(MYSQLI_ASSOC);
+        echo   '</article>';
+        echo '<article class="card">';
+        echo  ' <div class="card-int">';
+        echo '<span class="card__span">'.htmlspecialchars($row['id']).'</span>';
+        echo '<img src="img/productos/' . htmlspecialchars($row['foto']) . '" alt="Foto de ' . htmlspecialchars($row['nombre']) . '" class="img">';
+        echo '<div class="card-data">';
+        echo  '<h1 class="title">'.htmlspecialchars($row['nombre']).'</h1>';
+        echo  '<p> $'.htmlspecialchars($row['precio']).'</p>';
+        echo  '<p>'.htmlspecialchars($row['descripcion']).'</p>';
+       
+        echo   '<button class="button">Me interesa</button>';
+        echo   '</div>';
+        echo   '</div>';
+        echo   '</article>';
+    }
+
+    $result->close();
+    $conn->close();
+    ?>
+</section>
+
     </main>
 </body>
 <footer>
